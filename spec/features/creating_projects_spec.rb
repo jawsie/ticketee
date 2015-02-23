f8e5c5456a555d9824667cd4ff0f2275a5d4c68e
jantao <jantao@pinsightmedia.com>
2015-02-23 09:50:05 -0600

    Implement tickets display

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -11,7 +11,7 @@ RSpec.feature "Creating Projects" do
     expect(page).to have_content("Project has been created.")
     project = Project.find_by(name: "Sublime Text 3")
     expect(page.current_url).to eql(project_url(project))
-    title = "Sublime Text 3 - Projects - Ticketee"
+    title = "Ticketee"
     expect(page).to have_title(title)
   end
   scenario "A user cannot create a project without a name" do
