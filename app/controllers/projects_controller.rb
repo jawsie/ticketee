b84f6eb006ae11be5c332fc2fe0a344cf364d229
jantao <jantao@pinsightmedia.com>
2015-03-13 16:24:19 -0500

    Only admins can create or delete projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,28 +3,8 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
-
-  def new
-    @project = Project.new
-  end
-
-  def create
-    @project = Project.new(project_params)
-    if @project.save
-      flash[:notice] = "Project has been created."
-      redirect_to @project
-    else
-      flash.now[:alert] = "Project has not been created."
-      render "new"
-    end
-  end
-
-  def show
-  end
-
   def edit
   end
-
   def update
     if @project.update(project_params)
       flash[:notice] = "Project has been updated."
@@ -34,13 +14,6 @@ class ProjectsController < ApplicationController
       render "edit"
     end
   end
-
-  def destroy
-    @project.destroy
-    flash[:notice] = "Project has been deleted."
-    redirect_to projects_path
-  end
-
   private
   def set_project
     @project = Project.find(params[:id])
@@ -49,7 +22,6 @@ class ProjectsController < ApplicationController
         " for could not be found."
     redirect_to projects_path
   end
-
   def project_params
     params.require(:project).permit(:name, :description)
   end
